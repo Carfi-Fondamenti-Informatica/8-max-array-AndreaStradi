955dcf814169ed70ee22b81e91e570fcb820dff3
AndreaStradi <91668754+AndreaStradi@users.noreply.github.com>
2021-12-11 09:59:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+//
+// Created by andre on 11/12/2021.
+//
 
+#include "lib.h"
+float numeroMassimo (const float numeri [], int dim){
+    numeri [dim] ;
+    float massimo = numeri [0];
+    for (int i=0 ; i < dim ; i++){
+        if (massimo < numeri[1]){
+            massimo = numeri [1];
+        }
+    }
+    return massimo;
+}
