8f4f4c25b5bce377597af798cb96ef987bb61507
AndreaStradi <91668754+AndreaStradi@users.noreply.github.com>
2021-12-11 10:00:06 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by andre on 11/12/2021.
+//
 
+#ifndef ESERCITAZIONE_8_LIB_H
+#define ESERCITAZIONE_8_LIB_H
+float numeroMassimo (const float numeri[], int dimensione);
+
+#endif //ESERCITAZIONE_8_LIB_H
